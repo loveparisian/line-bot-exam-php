1ab92a918233dae95193f2d30fabf07b263230cc
loveparisian <58586769+loveparisian@users.noreply.github.com>
2019-12-06 15:19:14 +0700

    Update botpush.php
    edit access

--- botpush.php
+++ botpush.php
@@ -6,9 +6,9 @@ require "vendor/autoload.php";
 
 $access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = 'c87b737e228e85692b5f97ad1c5b435a';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'Uca06916089ac49d4baed3612353291e2';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
